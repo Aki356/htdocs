d126e0500167b03fcb98c31a2c397f43e26e1b5f
Aki356 <lyuba.glotova@yandex.ru>
2024-06-13 19:16:23 +0500

    изменила дизайн

--- js/js.js
+++ js/js.js
@@ -1,28 +1,56 @@
 (function($){
-$('.popular__items').slick({
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    autoplay: false,
-    autoplaySpeed: 2000,
-  });
-
-  $('.main-hall__items').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 2000,
-  });
 
   $('.slyder__photos').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     fade: true,
     cssEase: 'linear',
     dots: true,
     autoplaySpeed: 2000,
   });
-  // function addCart(){
-    
-  // }
+  var width = document.body.clientWidth; // ширина
+  console.log(width);
+
+  //var variable = false; // переменная, которую будем изменять
+
+  if(width >= 768){
+    $('.popular__items').slick({
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      autoplay: false,
+      autoplaySpeed: 2000,
+    });
+    $('.main-hall__items').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2000,
+    });
+  }
+  
+  if(width < 600) {
+    $('.popular__items').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2000,
+    });
+  }
+  if(width < 768) { // если ширина меньше 768...
+    $('.popular__items').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2000,
+    });
+    $('.main-hall__items').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2000,
+    });
+  }
+  
+
 })(jQuery);
